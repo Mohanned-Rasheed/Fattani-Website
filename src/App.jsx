94b555292befb54245175b8e3560e894031020d6
Mohanned-Rasheed <mohanned3211@gmail.com>
2024-04-28 18:25:32 +0300

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/about-us", element: <div>About us</div> },
+        { path: "/services", element: <div /> },
+        { path: "contact", element: <div /> },
+      ],
+    },
+  ]);
   return (
     <>
-      <div> hello</div>
+      <RouterProvider router={router} />
     </>
   );
 }
---
+++ src/pages/Root.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+function Root() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow mb-20">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Root;
